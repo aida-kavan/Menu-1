a93fb244f1909193861ca9e40e22bcbe58cdaa18
aida-kavan <aidahajikavan@gmail.com>
2024-11-08 19:49:31 +0330

    Add files via upload

---
+++ Menu1.js
@@ -0,0 +1,12 @@
+let WarmBtn = document.getElementById("WarmCategory")
+let ColdBtn = document.getElementById("ColdCategory")
+let TeaBtn = document.getElementById("TeaCategory")
+let DessertBtn = document.getElementById("dessertCategory")
+
+
+let WarmSection = document.getElementById("Warm_drinks")
+let ColdSection= document.getElementById("Cold-drinks")
+let TeaSection= document.getElementById("Tea")
+let DessertSection= document.getElementById("desserts")
+
+
